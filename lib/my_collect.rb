8847f868ab20b89689bc41667f7daea6a27bedc1
AJ Guiles <ashiguiles@gmail.com>
2019-03-03 07:50:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,21 @@
-# def my_collection(collection)
-#     i < 0
-#   new_collection = [] 
+def my_collection(collection)
+    i < 0
+  new_collection = [] 
  
-#   while i < collection.length
-#   new_collection << yield(collection[i])
-#   i += 1
-# end
-# new_collection
-# end
-
-def my_collect(collection)
-
-    x = 0
-    new_arr = []
-    while x < collection.length
-        new_arr << yield(collection[x])
-        x += 1
-    end
-    new_arr
+  while i < collection.length
+  new_collection << yield(collection[i])
+  i += 1
 end
+new_collection
+end
+
+# def my_collect(collection)
+
+#     x = 0
+#     new_arr = []
+#     while x < collection.length
+#         new_arr << yield(collection[x])
+#         x += 1
+#     end
+#     new_arr
+# end
