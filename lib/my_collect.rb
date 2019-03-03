b9d5a4fd1fb851b373e62b37f017431fb9ce2f43
AJ Guiles <ashiguiles@gmail.com>
2019-03-03 07:51:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,13 +9,3 @@ end
 new_collection
 end
 
-# def my_collect(collection)
-
-#     x = 0
-#     new_arr = []
-#     while x < collection.length
-#         new_arr << yield(collection[x])
-#         x += 1
-#     end
-#     new_arr
-# end
